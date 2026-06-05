133dc7e5c947bdb1f3a090c4d8bcfbf8a9805b54
jini5098 <jinijini5098@naver.com>
2026-06-06 08:55:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+    console.log('[Service Worker] Install 완료');
 });
+
+// 경고를 없애기 위해 기본 네트워크 요청을 그대로 통과시키는 코드 추가
 self.addEventListener('fetch', (e) => {
-    // 오프라인 캐싱 등은 생략하고 기본 통과
+    e.respondWith(fetch(e.request));
 });
